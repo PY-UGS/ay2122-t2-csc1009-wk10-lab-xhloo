cc175b2b60b146e6a7c0452185958cd7231b695d
xhloo <97490258+xhloo@users.noreply.github.com>
2022-03-28 14:23:49 +0800

    wk10 laboratory

---
+++ Q3a.py
@@ -0,0 +1,11 @@
+module = input("Enter a module code: ")
+
+switcher = {
+    'CSC1006':'Mathematics 2',
+    'CSC1007':'Operating System',
+    'CSC1008':'Data Structures and Algorithms',
+    'CSC1009':'Object-Oriented Programming',
+    'CSC1010':'Compurer Networks'
+}
+
+print(switcher[module])
