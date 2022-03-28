6e19fc0c9cb83054fcda059cfb9d0be0b7581231
xhloo <97490258+xhloo@users.noreply.github.com>
2022-03-28 15:55:50 +0800

    Delete Q2.py

--- Q2.py
+++
@@ -1,4 +0,0 @@
-input1 = float(input("Please enter your first number:\n"))
-input2 = float(input("Please enter your second number:\n"))
-average = (input1 + input2) / 2
-print("The average number of", input1, "and", input2, "is", average)
